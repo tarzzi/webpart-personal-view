07cec8146f19426c9439c4ce891fce0fa01ce7af
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2023-01-08 21:53:31 +0200

    Add mail view

--- src/webparts/webpartPersonalView/components/mail/MailItem.tsx
+++ src/webparts/webpartPersonalView/components/mail/MailItem.tsx
@@ -1,21 +1,47 @@
 // acts as a mail item component, that gets the mail item data from the graph api response
 import * as React from "react";
 import styles from "./MailItem.module.scss";
+import { Stack, IStackTokens } from "@fluentui/react/lib/Stack";
+import { Text } from "@fluentui/react/lib/Text";
 
 export default function MailItem(mailItem: any): JSX.Element {
   const item = mailItem.mailItem;
-  console.log(item);
+  const receieved = new Date(item.receivedDateTime).toLocaleString();
+
+  const stackTokens: IStackTokens = {
+    childrenGap: 5,
+    padding: 10,
+  };
+  const bodyPreviewSuffixed = item.bodyPreview.length < 250 ? item.bodyPreview : item.bodyPreview.substring(0, 250) + "..."; 
 
   return (
+    <Stack horizontal tokens={stackTokens} className={styles.container}>
+      <Stack.Item className={styles.info} disableShrink>
+        <Stack tokens={stackTokens}>
+          <Text className={styles.bold}> {item.sender.emailAddress.name} </Text>
+          <Text className={styles.italic}> {receieved} </Text>
+        </Stack>
+      </Stack.Item>
+      <Stack.Item align="center" tokens={stackTokens}>
+        <Text> {bodyPreviewSuffixed} </Text>
+      </Stack.Item>
+    </Stack>
+  );
+
+  /*   return (
     <div className={styles.container}>
       <a href={item.webLink} target="_blank" rel="noreferrer">
         <p>
-          {item.sender.emailAddress.name} - {item.receivedDateTime}
+          {item.sender.emailAddress.name} - {receieved}
         </p>
-        <p className={styles.bold}> {item.subject} </p>
-        <hr className={styles.line} />
+        <p className={styles.bold}> </p>
+        <Stack tokens={stackTokens}>
+          <Separator>
+            <Text className={styles.bold}>  {item.subject} </Text>
+          </Separator>
+        </Stack>
         <p className={styles.italic}> {item.bodyPreview} </p>
       </a>
     </div>
-  );
+  ); */
 }
