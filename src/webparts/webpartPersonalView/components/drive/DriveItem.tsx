5491d97daa31e7e13e38a6fcc38b159d86e411b1
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2023-01-15 14:30:19 +0200

    Add icon and onhover effects for drive cards

--- src/webparts/webpartPersonalView/components/drive/DriveItem.tsx
+++ src/webparts/webpartPersonalView/components/drive/DriveItem.tsx
@@ -5,16 +5,86 @@ import { Text } from "@fluentui/react/lib/Text";
 import { Stack } from "@fluentui/react/lib/Stack";
 import { IStackTokens } from "@fluentui/react/lib/Stack";
 import { Image } from "office-ui-fabric-react";
+import { Icon } from "@fluentui/react/lib/Icon";
+import { getFileTypeIconProps} from "@fluentui/react-file-type-icons";
+// import icons for mail, calendar, files and todo
+3
 
 export default function DriveItem(file: any): JSX.Element {
+  const lastUsed = file.file.lastUsed;
+  const lastAccessedDateTime = new Date(
+    lastUsed.lastAccessedDateTime
+  ).toLocaleString();
+  const lastModifiedDateTime = new Date(
+    lastUsed.lastModifiedDateTime
+  ).toLocaleString();
+  const lastSeenDateTime =
+    lastAccessedDateTime > lastModifiedDateTime
+      ? lastAccessedDateTime
+      : lastModifiedDateTime;
+
+  
   const url = file.file.resourceReference.webUrl;
   file = file.file.resourceVisualization;
+  let fileType: string = file.type;
+  fileType = fileType.toLowerCase();
+  // set icon based on file type
+
+  switch (fileType) {
+    case "pdf":
+      fileType = "pdf";
+      break;
+    case "excel":
+      fileType = "xls";
+      break;
+    case "word":
+      fileType = "doc";
+      break;
+    case "powerpoint":
+      fileType = "ppt";
+      break;
+    case "image":
+      fileType = "png";
+      break;
+    case "video":
+      fileType = "mp4";
+      break;
+    case "audio":
+      fileType = "mp3";
+      break;
+    case "text":
+      fileType = "txt";
+      break;
+    case "zip":
+      fileType = "zip";
+      break;
+    default:
+      fileType = "docx";
+      break;
+  }
+
   const stackTokens: IStackTokens = {
     childrenGap: 5,
-    padding: 10,
+  };
+  const imageStackTokens: IStackTokens = {
+    childrenGap: 5,
+  };
+  const textStackTokens: IStackTokens = {
+    padding: 15,
   };
 
-/*   //check if image is valid, if not, use a placeholder
+  // check if hovering over container to show preview image
+  const [hovering, setHovering] = React.useState(false);
+
+  const handleMouseEnter = () : void =>  {
+    setHovering(true);
+  };
+  const handleMouseLeave = () : void => {
+    setHovering(false);
+  };
+  
+
+  /*   //check if image is valid, if not, use a placeholder
 
   let a = document.createElement("a");
   a.href = file.previewImageUrl;
@@ -25,15 +95,24 @@ export default function DriveItem(file: any): JSX.Element {
   } */
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <Stack horizontal grow={3} tokens={stackTokens}>
-        <Stack.Item className={styles.fullWidth} >
+        <Stack.Item className={`${styles.fullWidth} ${styles.relative}`} tokens={textStackTokens} >
           <Stack tokens={stackTokens}>
-            <Text  className={styles.bold}>{file.title}</Text>
+            <Icon
+              {...getFileTypeIconProps({
+                extension: fileType,
+                size: 40,
+                imageFileType: "png",
+              })}
+              className={styles.fileIcon}
+            />
+            <Text className={styles.bold}>{file.title}</Text>
             <Text className={styles.italic}>{file.type}</Text>
+            <Text className={styles.italic}>Edited: {lastSeenDateTime}</Text>
           </Stack>
         </Stack.Item>
-        <Stack.Item align="end" tokens={stackTokens}>
+        <Stack.Item className={hovering ? styles.show : styles.hide} align="end" tokens={imageStackTokens}>
           <a href={url} target="_blank" rel="noreferrer">
             <Image
               onError={({ currentTarget }) => {
